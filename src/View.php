fdf16fec41ab925e82a373b1b1dd2da41743e6cf
Mark Ingman <mark.ingman@valhalla.software>
2024-10-14 15:52:38 +0100

    Remove deprecated get() method

--- src/View.php
+++ src/View.php
@@ -27,33 +27,6 @@ class View implements ViewInterface
 		}
 	}
 
-	public function get(string $view, array $_VARS = null, $assume_ob = true): mixed
-	{
-		// Deprecated
-
-		if (($_TEMPLATE = $this->find($view)) === false) {
-			return '';
-		}
-
-		$_VARS = (array)$_VARS;
-
-		ob_start();
-
-		$ret = call_user_func(
-			function () use ($_TEMPLATE, $_VARS) {
-				if ($_VARS) {
-					extract($_VARS, EXTR_REFS);
-				}
-
-				return require $_TEMPLATE;
-			}
-		);
-
-		$ob = (string)ob_get_clean();
-
-		return ($ret === 1 and $assume_ob) ? $ob : $ret;
-	}
-
 	public function fetch(string $path): Closure
 	{
 		if (!$realpath = $this->find($path)) {
--- src/ViewInterface.php
+++ src/ViewInterface.php
@@ -8,7 +8,5 @@ interface ViewInterface
 {
 	public function find(string $view): string|false;
 
-	public function get(string $view, array $_VARS = null): mixed;
-
 	public function fetch(string $path): Closure;
 }
